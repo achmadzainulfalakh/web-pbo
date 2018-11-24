11ad3c1195e2efe41d230c3577d176fc1eadb081
BokuNoCode <35252089+BokuNoCode@users.noreply.github.com>
2018-11-24 10:57:17 +0700

    Update Mlogin.php

--- Materi5-login-model-database/application/models/Mlogin.php
+++ Materi5-login-model-database/application/models/Mlogin.php
@@ -1,5 +1,5 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
-class Users_model extends CI_Model {
+class Mlogin extends CI_Model {
     public function __construct()
     {
         parent::__construct();
